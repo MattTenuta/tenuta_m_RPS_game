a8113ddc0e1f96bf005dd976069b44e70572dbfa
Matt Tenuta <m_tenuta@fanshaweonline.ca>
2021-10-21 15:37:59 -0400

    added logic

--- main.py
+++ main.py
@@ -13,3 +13,33 @@ computerLives = 5
 
 print("player chose: " + player)
 print("computer chose: " + computer)
+
+if (computer == player):
+    # tie - nothing else to compare, so it'll exit
+    print("tie! try again")
+
+elif (player == "rock"):
+    if (computer == "paper"):
+        print("you lose!")
+        playerLives = playerLives - 1
+    else:
+        print("you win!")
+        computerLives = computerLives - 1
+
+elif (player == "paper"):
+    if (computer == "scissors"):
+        print("you lose!")
+        playerLives = playerLives - 1
+    else:
+        print("you win!")
+        computerLives = computerLives - 1
+
+elif (player == "scissors"):
+    if (computer == "rock"):
+        print("you lose!")
+        playerLives = playerLives - 1
+    else:
+        print("you win!")
+        computerLives = computerLives - 1
+print("player life count: " + str(playerLives))
+print("computer life count: " + str(computerLives))
