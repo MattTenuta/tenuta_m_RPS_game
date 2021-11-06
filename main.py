731dc2626081c56de8680e017c0dfc1958dd4b0f
Matt Tenuta <m_tenuta@fanshaweonline.ca>
2021-11-06 13:44:58 -0400

    added =======

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from random import randint
 from gameComponents import winLose, gameVars, playingGame
 
 # set up pur game loop so that we can keep playing and not exit
+
+print("==============================")
+print("Welcome to Rock Paper Scissors OF DEATH!!! (Not Really)")
+
 while gameVars.player is False: 
-    gameVars.player = input("Choose your weapon: Rock, Paper or Scissors: ")
+    print("==============================")
+    gameVars.player = input("Choose your weapon: rock, paper or scissors: ")
+    print("==============================")
     gameVars.computer = gameVars.choices[randint(0, 2)]
 
     print("player chose: " + gameVars.player)
@@ -14,9 +20,11 @@ while gameVars.player is False:
     print("computer life count: " + str(gameVars.computerLives))
 
     if gameVars.playerLives == 0:
+        print("==============================")
         winLose.winorlose("Lost")
         
     elif gameVars.computerLives == 0:
+        print("==============================")
         winLose.winorlose("Won")
         
     gameVars.player = False
